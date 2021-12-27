541589f1f08b99df4240445f7dbca4418f516a82
Scott Schwab <schwabpiano@gmail.com>
2021-12-26 20:19:02 -0600

    Set up basic UI with CSS.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-alert("Working!");
+function add(a, b) {
+    return a + b;
+
+}
+
+function subtract(a, b) {
+    return a - b;
+
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operator (x, y, op) {
+    let result;
+    switch (op) {
+        case '+' :
+        result = add(x, y);
+        break;
+        case '-' :
+        result = subtract(x, y);
+        break;
+        case '*' :
+        result = (multiply(x, y));
+        break;
+        case '/' :
+        result = divide(x, y);
+        break;
+        default :
+        result = "error, not an operator.";
+    }
+
+    return result;
+}
+
+// alert(operator(12, 4, '/'));
